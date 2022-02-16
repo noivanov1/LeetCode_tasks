2fe951901a6142e43dadb80d0cc8cb02fa47e9d2
nikita <noivanov1@gmail.com>
2022-02-16 17:54:32 +0300

    task 190

---
+++ 190.Reverse_Bits.py
@@ -0,0 +1,14 @@
+# LeetCode:
+# 16.02.2022
+# Runtime: 41 ms, faster than 54.67% of Python3 online submissions for Reverse Bits.
+# Memory Usage: 13.9 MB, less than 69.54% of Python3 online submissions for Reverse Bits.
+
+def reverseBits(self, n: int) -> int:
+    if len(str(n)) != 32:
+        str_n = format(n, "b")
+    else:
+        str_n = str(n)
+    s = str_n[::-1]
+    while len(s) < 32:
+        s += '0'
+    return int(s, 2)
