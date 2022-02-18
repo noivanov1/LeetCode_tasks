18d88f43ebcf28849c759ffd986affda6aa398d6
nikita <noivanov1@gmail.com>
2022-02-18 17:17:37 +0300

    edit task 9

--- 9.Palindrome_Number.py
+++ 9.Palindrome_Number.py
@@ -1,3 +1,8 @@
+# LeetCode:
+# 18.02.2022
+# Runtime: 88 ms, faster than 49.00% of Python3 online submissions for Palindrome Number.
+# Memory Usage: 13.9 MB, less than 71.14% of Python3 online submissions for Palindrome Number.
+
 def isPalindrome(x: int) -> bool:
     for i in range(len(str(x)) // 2 + 1):
         if str(x)[i] != str(x)[-i - 1]:
