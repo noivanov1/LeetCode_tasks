bef78ff6719b718de29748d15840dc8b06b9f13b
nikita <noivanov1@gmail.com>
2022-02-23 00:13:55 +0300

    add task 709

---
+++ 709.To_Lower_Case.py
@@ -0,0 +1,11 @@
+# LeetCode:
+# 22.02.2022
+# Runtime: 32 ms, faster than 83.74% of Python3 online submissions for To Lower Case.
+# Memory Usage: 13.8 MB, less than 98.55% of Python3 online submissions for To Lower Case.
+
+def toLowerCase(s: str) -> str:
+    return s.lower()
+
+s = "Hello"
+
+print(toLowerCase(s))
