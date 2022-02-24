7e429c1a39a816e486feba0f04b0f050b9382753
nikita <noivanov1@gmail.com>
2022-02-24 19:53:30 +0300

    add task 35

---
+++ 35.Search_Insert_Position.py
@@ -0,0 +1,15 @@
+# LeetCode:
+# 24.02.2022
+# Runtime: 66 ms, faster than 55.46% of Python3 online submissions for Search Insert Position.
+# Memory Usage: 14.6 MB, less than 94.17% of Python3 online submissions for Search Insert Position.
+
+from typing import List
+
+
+def searchInsert(nums: List[int], target: int) -> int:
+    if target not in nums:
+        for i in range(len(nums)):
+            if target < nums[i]:
+                return i
+        return len(nums)
+    return nums.index(target)
