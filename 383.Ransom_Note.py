8fd84504a7d3277b57ea832abb17fe4d20c5f75e
nikita <noivanov1@gmail.com>
2022-02-26 00:56:45 +0300

    add task 383

--- 383.Ransom_Note.py
+++ 383.Ransom_Note.py
@@ -3,6 +3,7 @@
 # Runtime: 198 ms, faster than 5.14% of Python3 online submissions for Ransom Note.
 # Memory Usage: 14.4 MB, less than 20.45% of Python3 online submissions for Ransom Note.
 
+
 def canConstruct(ransomNote: str, magazine: str) -> bool:
     mag = [item for item in magazine]
     k = 0
